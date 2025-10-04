38c2f8131e506e967587a182a9af07674f0ae6db
NeboDev <yisusaq7@hotmail.com>
2025-10-03 18:46:28 -0600

    Ejemplos de JS02_Variables_Entradas_Operadores.pdf agregados

--- actividades/a06/src/main.js
+++ actividades/a06/src/main.js
@@ -15,3 +15,29 @@ function ejemplo1() {
   let div = document.getElementById("mensaje");
   div.innerHTML = "<p>Hola Mundo</p>";
 }
+
+function ejemplo2() {
+  var nombre = "Juan";
+  var edad = 10;
+  var altura = 1.92;
+  var casado = false;
+
+  var div1 = document.getElementById("nombre");
+  var div2 = document.getElementById("edad");
+  var div3 = document.getElementById("altura");
+  var div4 = document.getElementById("casado");
+  div1.innerHTML = "<p>" + nombre + "</p>";
+  div2.innerHTML = "<p>" + edad + "</p>";
+  div3.innerHTML = "<p>" + altura + "</p>";
+  div4.innerHTML = "<p>" + casado + "</p>";
+}
+
+function ejemplo3() {
+  var nombre;
+  var edad;
+  nombre = prompt("Ingresa tu nombre:", "");
+  edad = prompt("Ingresa tu edad:", "");
+  var div1 = document.getElementById("mensaje1");
+  div1.innerHTML =
+    "<p>Hola " + nombre + " asi que tienes " + edad + " años" + "</p>";
+}

85d6ff66268e940325aeeb0cdcd7ae5998f92d12
NeboDev <yisusaq7@hotmail.com>
2025-10-03 18:19:14 -0600

    Ejemplo de JS01_Introduccion_a_JavaScript.pdf agregado

--- actividades/a06/src/main.js
+++ actividades/a06/src/main.js
@@ -1,13 +1,17 @@
+function getDatos() {
+  var nombre = prompt("Nombre: ", "");
 
-function getDatos()
-{
-    var nombre = prompt("Nombre: ", "");
+  var edad = prompt("Edad: ", 0);
 
-    var edad = prompt("Edad: ", 0);
+  var div1 = document.getElementById("nombre");
+  div1.innerHTML = "<h3> Nombre: " + nombre + "</h3>";
 
-    var div1 = document.getElementById('nombre');
-    div1.innerHTML = '<h3> Nombre: '+nombre+'</h3>';
+  var div2 = document.getElementById("edad");
+  div2.innerHTML = "<h3> Edad: " + edad + "</h3>";
+}
 
-    var div2 = document.getElementById('edad');
-    div2.innerHTML = '<h3> Edad: '+edad+'</h3>';
+function ejemplo1() {
+  //El document write hacer que despues de renderizada la pagina por eso lo cambio a como se hace en la funcion de ejemplo getDatos()
+  let div = document.getElementById("mensaje");
+  div.innerHTML = "<p>Hola Mundo</p>";
 }

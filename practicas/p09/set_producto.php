301340f6482639756b927f269864cd7f288157ad
NeboDev <yisusaq7@hotmail.com>
2025-10-08 12:17:01 -0600

    Actualización de archivos punto 1

--- practicas/p09/set_producto.php
+++ practicas/p09/set_producto.php
@@ -5,28 +5,23 @@ $modelo = 'modelo_producto';
 $precio = 1.0;
 $detalles = 'detalles_producto';
 $unidades = 1;
-$imagen   = 'img/imagen.png';
+$imagen   = 'src/imagen.png';
 
 /** SE CREA EL OBJETO DE CONEXION */
-@$link = new mysqli('localhost', 'root', '12345678a', 'marketzone');	
+@$link = new mysqli('localhost', 'root', '1234567890', 'marketzone');
 
 /** comprobar la conexión */
-if ($link->connect_errno) 
-{
-    die('Falló la conexión: '.$link->connect_error.'<br/>');
+if ($link->connect_errno) {
+    die('Falló la conexión: ' . $link->connect_error . '<br/>');
     /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
 }
 
 /** Crear una tabla que no devuelve un conjunto de resultados */
 $sql = "INSERT INTO productos VALUES (null, '{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}')";
-if ( $link->query($sql) ) 
-{
-    echo 'Producto insertado con ID: '.$link->insert_id;
-}
-else
-{
-	echo 'El Producto no pudo ser insertado =(';
+if ($link->query($sql)) {
+    echo 'Producto insertado con ID: ' . $link->insert_id;
+} else {
+    echo 'El Producto no pudo ser insertado =(';
 }
 
 $link->close();
-?>
